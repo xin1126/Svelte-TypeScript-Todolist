e667f134d32edddd188a31cec4e025d7a7a5adb2
xin1126 <z26319491z@gmail.com>
2022-10-10 00:08:45 +0800

    fix: 清除多餘檔案

--- src/components/Counter.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	let count = 0;
-</script>
-
-<div class="p-10">
-	<input class="text-indent-3" type=number bind:value={count}>
-	<button class="ml-3" on:click="{() => count += 1}">count</button>
-</div>
